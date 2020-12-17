db655c063bc09354eadabdac769461c9b3f1fe72
mclpne <justin.macalpine@gmail.com>
2020-12-16 16:27:24 -0800

    Fixed mailgun auth object

--- mail.js
+++ mail.js
@@ -5,7 +5,7 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: process.env.API_KEY,
+        apiKey: process.env.API_KEY,
         domain: process.env.DOMAIN
     }
 };
