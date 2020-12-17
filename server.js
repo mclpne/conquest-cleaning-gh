9f01ee6d56564c0ed786d70225f34e2112cc4fed
mclpne <justin.macalpine@gmail.com>
2020-12-17 14:07:03 -0800

    Changed PORT variable for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const sendMail = require('./mail.js');
 const app = express();
 const path = require('path');
 
+const PORT = process.env.PORT || 80;
+
 app.use(express.urlencoded({
     extended: false
 }));
@@ -28,4 +30,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
-// app.listen(PORT, () => console.log('Server is starting on PORT ', PORT));
+app.listen(PORT, () => console.log('Server is starting on PORT ', PORT));
