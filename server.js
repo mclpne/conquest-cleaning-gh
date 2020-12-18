9aae9bb9ebcefe2678d17850d71dfdc3a73f1eee
mclpne <justin.macalpine@gmail.com>
2020-12-18 15:12:45 -0800

    Changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sendMail = require('./mail.js');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 80;
+const PORT = 5000;
 
 app.use(express.urlencoded({
     extended: false
